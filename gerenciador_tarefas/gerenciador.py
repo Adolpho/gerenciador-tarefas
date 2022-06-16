8b962516335c74bc2ecc349e45979492a3da4368
Adolpho <adolpho.rodrigues@gmail.com>
2022-06-16 14:04:13 -0300

    Adiciona integração contínua

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
 # ...
 TAREFAS = [
     {
@@ -9,9 +8,22 @@ TAREFAS = [
         "titulo": "titulo 1",
         "descricao": "descricao 1",
         "estado": "finalizado",
-    }
+    },
+    {
+        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "titulo": "titulo 2",
+        "descricao": "descricao 2",
+        "estado": "aberto",
+    },
+    {
+        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "titulo": "titulo 3",
+        "descricao": "descricao 3",
+        "estado": "processado",
+    },
 ]
 # ...
+
 
 @app.get("/tarefas")
 def listar():
